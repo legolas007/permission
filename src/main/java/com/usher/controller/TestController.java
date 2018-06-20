50e3b95c9abb7f3e73327939fa265b4f18ce467b
legolas007 <oukuanou@163.com>
2018-06-20 15:45:07 +0800

    工具类

--- src/main/java/com/usher/controller/TestController.java
+++ src/main/java/com/usher/controller/TestController.java
@@ -1,6 +1,15 @@
 package com.usher.controller;
 
 
+import com.usher.common.ApplicationContextHelper;
+import com.usher.common.JsonData;
+import com.usher.dao.SysAclModuleMapper;
+import com.usher.exception.ParamException;
+import com.usher.exception.PermissionException;
+import com.usher.model.SysAclModule;
+import com.usher.param.TestVo;
+import com.usher.util.BeanValidator;
+import com.usher.util.JsonMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +20,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Slf4j
 public class TestController {
 
-    //@RequestMapping("/hello.json")
+    @RequestMapping("/hello.json")
     @ResponseBody
-    public String hello() {
+    public JsonData hello() {
         log.info("hello");
-      //  throw new PermissionException("test exception");
-         return ("hello, permission");
+        throw new PermissionException("test exception");
+        // return JsonData.success("hello, permission");
     }
 
+    @RequestMapping("/validate.json")
+    @ResponseBody
+    public JsonData validate(TestVo vo) throws ParamException {
+        log.info("validate");
+        SysAclModuleMapper moduleMapper = ApplicationContextHelper.popBean(SysAclModuleMapper.class);
+        SysAclModule module = moduleMapper.selectByPrimaryKey(1);
+        log.info(JsonMapper.obj2String(module));
+        BeanValidator.check(vo);
+        return JsonData.success("test validate");
+    }
 }
